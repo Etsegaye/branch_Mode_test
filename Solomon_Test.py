90dcd058c3e78ea17fb29a0940618ef54a281eb6
tesgaye <tsegaye.eshete@gmail.com>
2023-11-16 09:43:14 +0100

    remove some data

--- Solomon_Test.py
+++ Solomon_Test.py
@@ -3,5 +3,5 @@ import os
 print ( " TEST ")
 # what coms next
 print( " add some thing")
-print( " second change add some thing")
+print( " second  thing")
 print( " repeate second change add some thing")
